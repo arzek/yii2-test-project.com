b911282407dc553fda5359b3173a0a7901edaa19
Artem <arzek2015@gmail.com>
2016-04-16 13:22:57 +0300

    human-friendly URL

--- config/web.php
+++ config/web.php
@@ -38,14 +38,15 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                'login' => 'site/login',
             ],
-        ],
-        */
+        ]
     ],
     'params' => $params,
 ];
