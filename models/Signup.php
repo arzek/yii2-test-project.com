f832599e1a109f3a92c39d7d932e3f52ea496b28
Artem <arzek2015@gmail.com>
2016-04-17 14:51:30 +0300

    Validation

--- models/Signup.php
+++ models/Signup.php
@@ -11,4 +11,17 @@ class Signup extends Model
     public $number;
     public $referral;
     public $password;
+
+    public function rules()
+    {
+        return [
+            [['email','password','number'],'required'],
+            ['email','email'],
+            ['email','unique','targetClass' => 'app\models\User'],
+            ['number','number','min' => 9],
+            ['referral','number'],
+            ['password','string', 'min' => 6]
+
+        ];
+    }
 }
