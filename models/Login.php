5a00cee8578e74fb6a6cea1bc9e936985085a136
Artem <arzek2015@gmail.com>
2016-04-17 16:28:45 +0300

    Training Login

---
+++ models/Login.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 17.04.2016
+ * Time: 16:26
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+class Login extends Model
+{
+    public $email;
+    public $password;
+}
