319f18618c7a5a5f6fcfe5c49985268cb7fc5eea
Artem <arzek2015@gmail.com>
2016-04-23 21:07:35 +0300

    Fix single User

--- views/user/single.php
+++ views/user/single.php
@@ -1,3 +1,18 @@
-<h1><?= $user['name'] ?></h1>
-<p><?= $user['number'] ?></p>
-<p><?= $user['email'] ?></p>
+<table class="table">
+    <tr >
+        <td class="col-md-2"><h4>Name:</h4> </td>
+        <td><h4><?= $user['name'] ?></h4></td>
+    </tr>
+    <tr>
+        <td><h4>Number:</h4></td>
+        <td><h4><?= $user['number'] ?></h4></td>
+    </tr>
+    <tr>
+        <td><h4>Email:</h4></td>
+        <td><h4><?= $user['email'] ?></h4></td>
+    </tr>
+
+</table>
+
+
+
